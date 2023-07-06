15d34a27414a2b90883fa755a19117ecac12a6dd
Amarvenkat <amarvenkat30@gmail.com>
2023-07-06 11:58:27 +0530

    Textfields, Buttons & Showing Snackbars

--- app/src/main/java/com/example/stateapplication/MainActivity.kt
+++ app/src/main/java/com/example/stateapplication/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.stateapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Snackbar
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.stateapplication.ui.theme.StateApplicationTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,9 +27,49 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    Greeting("Android")
                 }
-                Snackbar {
-                    Text(text = "Hello")
+
+                val scaffoldState = rememberScaffoldState()
+
+                var textFieldState by remember {
+                    mutableStateOf("")
                 }
+                val scope = rememberCoroutineScope()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        TextField(
+                            value = textFieldState,
+                            label = {
+                                    Text(text = "Type to Show Toast")
+                            },
+                            onValueChange = {
+                                    textFieldState = it
+                            },
+                            singleLine = true,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Button(onClick = {
+                            scope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar("Toast $textFieldState")
+                            }
+
+                        }) {
+                            Text(text = "Show Toast")
+                        }
+                        
+                    }
+                }
+
             }
         }
     }
